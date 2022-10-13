1a9775226e3ceb4bf5b1f0e653b9dabed7592a6b
Tyler Ramos <60799532+TylerRamos@users.noreply.github.com>
2022-10-12 21:14:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ class Spikeball:
     def __init__(self):
         self.session = requests.Session()
         self.headers = {
-        "accept": "application/json, text/javascript, */*; q=0.01",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "en-US,en;q=0.9",
-        "dnt": "1",
-        "referer": "https://spikeball.com/collections/pro-set/products/dunkin",
-        "sec-ch-ua": '''"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"''',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '''"Windows"''',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
-        "x-requested-with": "XMLHttpRequest",
-    }
+            "accept": "application/json, text/javascript, */*; q=0.01",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "en-US,en;q=0.9",
+            "dnt": "1",
+            "referer": "https://spikeball.com/collections/pro-set/products/dunkin",
+            "sec-ch-ua": '''"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"''',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '''"Windows"''',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+            "x-requested-with": "XMLHttpRequest",
+        }
         
     def pull_stock(self, product_url):
         response = self.session.request("GET", product_url, headers=self.headers)
